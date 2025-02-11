745e0362ef86fee636fea42a5429497541b5ca90
Hilltuy <134991119+Hilltuy@users.noreply.github.com>
2025-02-11 13:16:19 +1100

    tuesday midi nit stuff

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
+import pygame.midi as midi
 
 pygame.init()
-
+midi.init()
 mainClock = pygame.time.Clock()
 
 SCREEN_WIDTH = 1200
@@ -9,8 +10,16 @@ SCREEN_HEIGHT = 960
 
 pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
+midiDataList = []
+midi_in = midi.Input(1) #can change the number to the computer keyboard for working at school
+def rightHandImprov():
+    print(midi.get_device_info(1)) #midi ID 1 is my midi controller keyboard
+    midiDataList.append(midi.Input.read(midi_in,1)) #reads in the midi input data
+    print(midiDataList) #displays the midiDataList table to prove that information is coming in
+
 running = True
 while running:
+    rightHandImprov()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -19,3 +28,10 @@ while running:
     mainClock.tick(60)
 
 running = False
+
+
+
+
+
+
+
