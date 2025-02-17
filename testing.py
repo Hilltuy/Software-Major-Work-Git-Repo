6e626bda69ebdcdeace7e53024586804423b0399
Hilltuy <billy.parsons1@education.nsw.gov.au>
2025-02-18 10:50:31 +1100

    1050

---
+++ testing.py
@@ -0,0 +1,26 @@
+import pygame
+import pygame.midi as midi
+import pygame.mixer as mixer
+
+pygame.init()
+midi.init()
+mixer.init()
+mainClock = pygame.time.Clock()
+#midi_in = midi.Input(1)
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 960
+
+pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+running = True
+while running:
+    midi.Output.write_short(0x90, 65, 100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    pygame.display.update()
+    mainClock.tick(60)
+
+running = False
