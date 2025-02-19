eb8fe007eb4b4e0b9dc296c960ae31a79af61d40
Hilltuy <134991119+Hilltuy@users.noreply.github.com>
2025-02-19 18:25:59 +1100

    got midi output sound to work

--- main.py
+++ main.py
@@ -10,14 +10,23 @@ SCREEN_HEIGHT = 960
 
 pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
-midiDataList = []
-midi_in = midi.Input(1) #can change the number to the computer keyboard for working at school
-def rightHandImprov():
-    print(midi.get_device_info(1)) #midi ID 1 is my midi controller keyboard
-    midiDataList.append(midi.Input.read(midi_in,1)) #reads in the midi input data
-    print(midiDataList[0]) #displays the midiDataList table to prove that information is coming in
 
+
+midiDataList = []
+midi_in = midi.Input(midi.get_default_input_id()) #can change the number to the computer keyboard for working at school
+#midi_out = midi.Output(midi.get_default_output_id(),0)
+midi_out = midi.Output(5,0)
+midi_out.set_instrument(0)
+
+def rightHandImprov():
+    #print(midi.get_device_info(1)) #midi ID 1 is my midi controller keyboard
+    #print(midi_in.read(1)[0])
+    midi_out.write(midi_in.read(1))
+    pass
+for i in range(-2, 8):
+    print(str(midi.get_device_info(i))+": "+str(i))
 running = True
+
 while running:
     rightHandImprov()
     for event in pygame.event.get():
@@ -25,7 +34,7 @@ while running:
             pygame.quit()
 
     pygame.display.update()
-    mainClock.tick(60)
+    #mainClock.tick(360)
 
 running = False
 
